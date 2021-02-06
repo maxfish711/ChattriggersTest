3c52e0496ee2436b42b66cb4dd547a0d76459fac
max <maxifishi1234567899@gmail.com>
2021-02-06 13:03:09 -0800

    added renderer funtion

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+var text=new Text("john",120,120).setColor(Renderer.color(100,75,200))
+var circle=new Shape(Renderer.color(200,100,50)).setCircle(140,150,60,360)
 function exampleWorldLoad() {
 ChatLib.chat('lol');
 }
-
+function renderoverlay(){
+	Renderer.drawString("bob the builder",100,100)
+	text.draw()
+	circle.draw()
+}
+register("renderOverlay", renderoverlay)
 register("worldLoad", exampleWorldLoad);
