c3fc5617b3c91edd324de9a4e715a9c32ea83af1
max <maxifishi1234567899@gmail.com>
2021-02-19 12:04:43 -0800

    add chat receved funtion

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 var text=new Text("john",120,120).setColor(Renderer.color(100,75,200))
+var clickmessage=new Message(
+	"this is not clickable",
+	new TextComponent("this is clickable").setClick(
+		"run_command",
+		"/gamemode 1"),
+	"!")
+var hovermessage=new TextComponent("hover over me").setHoverValue("u see me now")
 var circle=new Shape(Renderer.color(200,100,50)).setCircle(140,150,60,360)
 function exampleWorldLoad() {
+	console.log("yellow")
 ChatLib.chat('lol');
 }
 function renderoverlay(){
@@ -8,5 +16,12 @@ function renderoverlay(){
 	text.draw()
 	circle.draw()
 }
-register("renderOverlay", renderoverlay)
+//register("renderOverlay", renderoverlay)
 register("worldLoad", exampleWorldLoad);
+ChatLib.chat(clickmessage)
+ChatLib.chat(hovermessage)
+function onChatReceived(text,event){
+	var message=ChatLib.getChatMessage(event)
+	console.log(message)
+}
+register("messageSent",onChatReceived)
